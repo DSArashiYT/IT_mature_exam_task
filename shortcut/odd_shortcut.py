968db256d5e95e43c764ee50d1c90ee50419c87c
ArashiYT <79902847+ArashiYT@users.noreply.github.com>
2024-09-06 22:23:22 +0200

    Tests and Implement Code: OddShortcutNumber

---
+++ shortcut/odd_shortcut.py
@@ -0,0 +1,35 @@
+class OddShortcutNumber: 
+    __isNegativeNumber = False
+    __oddBase: int = 1
+    __inputNumber: int
+    __result: int = 0
+    
+    def of(self, number: int) -> int:
+        self.__resetBaseAndResult()
+        self.__checkNumber(number)
+        self.__saveNumber(number)
+        self.__extractOddDigitsOfNumber(10)
+
+        return self.__result * (-1 if self.__isNegativeNumber else 1)
+
+    def __saveNumber(self, number: int) -> None:
+        self.__inputNumber = abs(number)
+
+    def __checkNumber(self, __number: int) -> None:
+        self.__isNegativeNumber = (__number < 0)
+
+    def __resetBaseAndResult(self) -> None:
+        self.__result = 0
+        self.__oddBase = 1
+
+    def __extractOddDigitsOfNumber(self, base: int) -> None:
+        while self.__inputNumber != 0:
+            current_digit: int = self.__inputNumber % base
+            
+            if current_digit % 2 != 0:  # Sprawdzenie, czy cyfra jest nieparzysta
+                self.__result += current_digit * self.__oddBase
+                self.__oddBase *= base
+
+            self.__inputNumber //= base  # Skrócone przypisanie dzielenia
+
+    
---
+++ shortcut/test_odd_shortcut.py
@@ -0,0 +1,42 @@
+from odd_shortcut import OddShortcutNumber
+import unittest
+
+class OddShortcutNumberTest(unittest.TestCase):
+    def setUp(self):
+        self.instance = OddShortcutNumber()
+    
+    def test_of_method_with_odd_numbers(self):
+        self.assertEqual(self.instance.of(135), 135)
+        self.assertEqual(self.instance.of(395), 395)
+        self.assertEqual(self.instance.of(1), 1)
+
+    def test_of_method_with_mix_numbers(self):
+        self.assertEqual(self.instance.of(13522), 135)
+        self.assertEqual(self.instance.of(34125), 315)
+        self.assertEqual(self.instance.of(24432), 3)
+
+    def test_of_method_with_even_numbers(self):
+        self.assertEqual(self.instance.of(22242), 0)
+        self.assertEqual(self.instance.of(0), 0)
+        self.assertEqual(self.instance.of(222222), 0)
+
+    def test_of_method_with_large_number(self):
+        self.assertEqual(self.instance.of(1234567890123456789), 1357913579)
+        self.assertEqual(self.instance.of(111112222233221), 11111331)
+        self.assertEqual(self.instance.of(22200221112221111333),1111111333)
+
+    def test_of_method_with_negative_number(self):
+        self.assertEqual(self.instance.of(-13522), -135)
+        self.assertEqual(self.instance.of(-2344), -3)
+        self.assertEqual(self.instance.of(-2), 0)
+
+    def test_of_method_with_invalid_input(self):
+        with self.assertRaises(TypeError):
+            self.instance.of("abc")
+
+        with self.assertRaises(TypeError):
+            self.instance.of(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
